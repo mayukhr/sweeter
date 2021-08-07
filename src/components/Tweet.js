beea9f41b96c2ec12aa7bd66da551c1bf66d5472
Mayukh Roy <roy.mayukh@yahoo.in>
2021-08-08 00:39:40 +0530

    js only

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,9 +1,8 @@
 import React from "react";
 import styles from './Tweet.module.css';
 
-//TODO: add a key!!!!
 const Tweet = ({ json }) => {
-  const { data: {author_id, text}, includes: {users:[user]} } = json || {};
+  const { data: {text}, includes: {users:[user]} } = json || {};
   const {created_at, name, profile_image_url, username} = user || {};
   console.log(json);
 

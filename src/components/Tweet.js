c0e26b6cd521bbcaf6a67554c04975f88c7da1ed
Mayukh Roy <roy.mayukh@yahoo.in>
2021-08-08 13:13:39 +0530

    date formatted as requirement

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,10 +1,10 @@
-import React from "react";
 import styles from './Tweet.module.css';
+import moment from 'moment';
 
 const Tweet = ({ json }) => {
   const { data: {text}, includes: {users:[user]} } = json || {};
   const {created_at, name, profile_image_url, username} = user || {};
-  console.log(json);
+  const tweeted_at = moment(created_at).format('MM/DD/yyyy HH:mm:ss');
 
   return (
     <div className={styles.tweet}>
@@ -14,7 +14,7 @@ const Tweet = ({ json }) => {
       <p className={styles.tweetText}>{text}</p>
       <div className={styles.tweetDetails}>
         <span>Tweeted by: {name}</span>
-        <span className={styles.tweetedAt}>Tweeted at: {created_at}</span>
+        <span className={styles.tweetedAt}>Tweeted at: {tweeted_at}</span>
       </div>
     </div>
   )
